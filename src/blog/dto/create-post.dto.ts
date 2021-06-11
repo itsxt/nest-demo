b7a28cad6bca1781334d2e46cc2be9baebb93d83
itsxt <lixuetao197@163.com>
2021-06-11 18:25:18 +0800

    commit

--- src/blog/dto/create-post.dto.ts
+++ src/blog/dto/create-post.dto.ts
@@ -1,3 +1,9 @@
+/*
+ * @Author: itsxt
+ * @Date: 2021-06-11 18:24:55
+ * @Last Modified by:   itsxt
+ * @Last Modified time: 2021-06-11 18:24:55
+ */
 export class CreatePostDTO {
   readonly title: string;
   readonly description: string;
